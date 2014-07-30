bd80f61435fb775a1875760a34e870cbc24a3140
engone nze davy <engonenzedavy@yahoo.com>
2014-07-30 02:41:56 +0200

    cleaned the repo

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('philosAngularApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
