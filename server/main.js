8ff353fdd143abe82cd44dea665125830a4999f5
engone nze davy <engonenzedavy@yahoo.com>
2014-08-03 06:22:36 +0200

    added server configuration

--- server/main.js
+++ server/main.js
@@ -1,2 +1,20 @@
 var express = require('express'),
 	app = express();
+
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var config = require('./config/config')(env);
+
+require('./config/express')(app, config);
+require('./config/mongoose')(config);
+require('./config/routes')(app);
+
+// if (env === 'development') {
+// 	console.log(config.port);
+// 	module.exports = app;	
+// }else{
+// 	app.listen(config.port);
+// }
+
+
+app.listen(config.port);
