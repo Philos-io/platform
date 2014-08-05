d20a745821e053615a7a32df0ab9d3408590c75f
engone nze davy <engonenzedavy@yahoo.com>
2014-08-05 19:19:57 +0200

    added views and controller for register and workshops

---
+++ app/scripts/controllers/workshopsController.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/*
+* Workshops controller
+*/
+
+function workshopsController($scope) {
+	$scope.pageClass = 'page-login';
+}	
+
+angular.module('philosAngularApp').controller('WorkshopsController', workshopsController);
