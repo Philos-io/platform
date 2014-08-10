20e2a9e7293fed7436a9b985ff7064c56b249e4d
engone nze davy <engonenzedavy@yahoo.com>
2014-08-11 01:02:07 +0200

    fixed errors on the server/main

--- server/main.js
+++ server/main.js
@@ -6,16 +6,16 @@ var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var config = require('./config/config')(env);
 
 require('./config/express')(app, config);
-require('./config/mongoose')(config);
+// require('./config/mongoose')(config);
 require('./config/routes')(app);
 
-console.log(config.port);
+// console.log(config.port);
 
-if (env === 'development') {
-	module.exports = app;	
-}else{
-	app.listen(config.port);
-}
+// if (env === 'development') {
+// 	module.exports = app;	
+// }else{
+// 	app.listen(config.port);
+// }
 
 
-//app.listen(config.port);
+app.listen(config.port);
