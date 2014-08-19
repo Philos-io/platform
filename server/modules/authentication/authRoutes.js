9ee650548feeb237a2c7bcf3a4a24712cb7b9d9e
engone nze davy <engonenzedavy@yahoo.com>
2014-08-20 01:04:54 +0200

    added twitter authentication

--- server/modules/authentication/authRoutes.js
+++ server/modules/authentication/authRoutes.js
@@ -10,7 +10,8 @@ module.exports = function(app, passport){
 
 	app.get('/auth/twitter', passport.authenticate('twitter'));
 
-	app.get('/auth/twitter/callback', AuthFactory.signupWithTwitter);
+	app.get('/auth/twitter/callback', passport.authenticate('twitter', { successRedirect: '/',
+                                     failureRedirect: '/#/signIn' }));
 
 	app.get('/logout', AuthFactory.logout);
 }
